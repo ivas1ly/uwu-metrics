157c52bf326e5e9f0235e7482d1cafa6cf5e45c8
Vasily Yenin <vasily.yenin@gmail.com>
2023-11-05 21:54:34 +0700

    feat: add error message to agent send request logs

--- internal/agent/client.go
+++ internal/agent/client.go
@@ -34,14 +34,14 @@ func (c *Client) sendRequest(method, path string) error {
 	c.Logger.Debug(c.URL + path)
 	req, err := http.NewRequest(method, c.URL+path, nil)
 	if err != nil {
-		c.Logger.Error("can't create new request")
+		c.Logger.Error("can't create new HTTP request", slog.String("error", err.Error()))
 		return err
 	}
 	req.Header.Set("Content-Type", "text/plain")
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		c.Logger.Error("can't make the request")
+		c.Logger.Error("can't send the HTTP request", slog.String("error", err.Error()))
 		return err
 	}
 	defer resp.Body.Close()
