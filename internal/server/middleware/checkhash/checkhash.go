bbfb19acca8d4c2f8f1c6b626dc245e15725305a
Vasily Yenin <vasily.yenin@gmail.com>
2023-12-24 00:02:42 +0700

    feat: check the request hash and add the response body hash to the header on the server

---
+++ internal/server/middleware/checkhash/checkhash.go
@@ -0,0 +1,73 @@
+package checkhash
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+
+	"github.com/go-chi/render"
+	"go.uber.org/zap"
+
+	"github.com/ivas1ly/uwu-metrics/internal/lib/hash"
+)
+
+func New(log *zap.Logger, key []byte) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		l := log.With(zap.String("middleware", "check hash"))
+
+		l.Info("added check hash middleware")
+
+		checkHashFn := func(w http.ResponseWriter, r *http.Request) {
+			if len(key) == 0 {
+				l.Info("key is empty, skip hash check")
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			hashHeader := r.Header.Get("HashSHA256")
+			l.Info("hash", zap.String("header", hashHeader))
+
+			if hashHeader == "" {
+				l.Info("hash header is empty, skip check")
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			buf, err := io.ReadAll(r.Body)
+			if err != nil {
+				l.Info("can't read body")
+
+				w.WriteHeader(http.StatusBadRequest)
+				render.JSON(w, r, render.M{"message": "can't read body"})
+				return
+			}
+
+			sign, err := hash.New(buf, key)
+			if err != nil {
+				l.Info("can't get hash sign")
+
+				w.WriteHeader(http.StatusBadRequest)
+				render.JSON(w, r, render.M{"message": "can't check hash"})
+				return
+			}
+			l.Info("hash", zap.String("value", sign))
+
+			if sign != hashHeader {
+				l.Info("computed hash doesn't match the one provided in the HashSHA256 header")
+
+				w.WriteHeader(http.StatusBadRequest)
+				render.JSON(w, r, render.M{"message": "can't check hash"})
+				return
+			}
+
+			l.Info("hash check OK", zap.String("header", hashHeader), zap.String("sign", sign))
+
+			reader := io.NopCloser(bytes.NewBuffer(buf))
+			r.Body = reader
+
+			next.ServeHTTP(w, r)
+		}
+
+		return http.HandlerFunc(checkHashFn)
+	}
+}
--- internal/server/route.go
+++ internal/server/route.go
@@ -7,18 +7,24 @@ import (
 
 	"github.com/ivas1ly/uwu-metrics/internal/lib/postgres"
 	"github.com/ivas1ly/uwu-metrics/internal/server/handlers"
+	"github.com/ivas1ly/uwu-metrics/internal/server/middleware/checkhash"
 	"github.com/ivas1ly/uwu-metrics/internal/server/middleware/decompress"
 	"github.com/ivas1ly/uwu-metrics/internal/server/middleware/reqlogger"
+	"github.com/ivas1ly/uwu-metrics/internal/server/middleware/sethash"
 	"github.com/ivas1ly/uwu-metrics/internal/server/storage/memory"
 )
 
-func NewRouter(ms memory.Storage, db *postgres.DB, log *zap.Logger) *chi.Mux {
+func NewRouter(ms memory.Storage, db *postgres.DB, key string, log *zap.Logger) *chi.Mux {
 	router := chi.NewRouter()
 
 	router.Use(middleware.Compress(defaultCompressLevel))
 	// an error occurs here, can't use "middleware" package name for my own middlewares
 	router.Use(decompress.New(log))
 	router.Use(reqlogger.New(log))
+	if key != "" {
+		router.Use(checkhash.New(log, []byte(key)))
+		router.Use(sethash.New(log, []byte(key)))
+	}
 
 	handlers.NewRoutes(router, ms, log)
 
