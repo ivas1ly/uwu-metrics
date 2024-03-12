b479eee53d45c3147b3ac1239e4000a5b9ee1007
Vasily Yenin <vasily.yenin@gmail.com>
2024-03-13 02:17:03 +0700

    feat: print build version, build date and build commit at application startup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/ivas1ly/uwu-metrics/internal/server"
 )
 
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
 func main() {
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
+
 	cfg := server.NewConfig()
 
 	server.Run(cfg)
