2f9feed67ce0f642c62f2f8aa3b7535300237fb3
Vasily Yenin <vasily.yenin@gmail.com>
2024-03-25 01:03:53 +0700

    feat: add asymmetric encryption support with rsa to agent and server

--- internal/server/route.go
+++ internal/server/route.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"crypto/rsa"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"go.uber.org/zap"
@@ -10,17 +12,19 @@ import (
 	"github.com/ivas1ly/uwu-metrics/internal/server/middleware/checkhash"
 	"github.com/ivas1ly/uwu-metrics/internal/server/middleware/decompress"
 	"github.com/ivas1ly/uwu-metrics/internal/server/middleware/reqlogger"
+	"github.com/ivas1ly/uwu-metrics/internal/server/middleware/rsadecrypt"
 	"github.com/ivas1ly/uwu-metrics/internal/server/middleware/sethash"
 	"github.com/ivas1ly/uwu-metrics/internal/server/storage/memory"
 )
 
 // NewRouter creates a new HTTP router and adds common middlewares for all handlers.
-func NewRouter(ms memory.Storage, db *postgres.DB, key string, log *zap.Logger) *chi.Mux {
+func NewRouter(ms memory.Storage, db *postgres.DB, key string, privateKey *rsa.PrivateKey, log *zap.Logger) *chi.Mux {
 	router := chi.NewRouter()
 
 	router.Use(middleware.Compress(defaultCompressLevel))
 	// an error occurs here, can't use "middleware" package name for my own middlewares
 	router.Use(decompress.New(log))
+	router.Use(rsadecrypt.New(log, privateKey))
 	router.Use(reqlogger.New(log))
 	if key != "" {
 		router.Use(checkhash.New(log, []byte(key)))
